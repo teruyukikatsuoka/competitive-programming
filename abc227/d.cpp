8d257c85ac8b20f03d06fda1dc42acd666aa0dae
Soni <84605786+Soni-japan@users.noreply.github.com>
2021-11-16 02:35:24 +0900

    Create d.cpp

---
+++ abc227/d.cpp
@@ -0,0 +1,63 @@
+//---------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+#include <cstdlib>
+#include <cmath>
+#include <math.h>
+#include <sstream>
+#include <numeric>
+#include <cassert>
+//---------------------------------------------------------------
+#define rep(i, n) for (long long i = 0; i < (long long)(n); i++)
+#define rrep(i, n) for (long long i = 1; i <= (long long)(n); i++)
+#define srep(i, s, n) for (long long i = (s); i <= (long long)(n); i++)
+#define drep(i,n) for(long long i = (n)-1; i >= 0; --i)
+#define SIZE 100005
+#define INF 1e18
+#define all(x) x.begin(),x.end()
+#define fi first
+#define se second
+#define vec vector
+using pint = pair<int,int>;
+using ll = long long;
+using vll = vector<long long>;
+using vvll = vector<vector<long long>>;
+using pll = pair<long long, long long>;
+//---------------------------------------------------------------
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+//---------------------------------------------------------------
+struct Edge {
+    ll to;
+    ll w;
+    Edge(ll to, ll w) : to(to), w(w) {}
+};
+using Graph = vector<vector<Edge>>;
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    ll n,k;
+    cin>>n>>k;
+    vll a(n);
+    for(ll &x: a){cin>>x;}
+
+    ll l = -1;
+    ll r = 1e18/k;//kで割らないとWAする
+
+    while(r - l > 1){
+        ll mid = (l+r)/2,sum = 0; //立てられるプロジェクト、プロジェクトに派遣される合計人数(多いほうがいい)
+        for(ll x: a){
+            sum += min(mid,x);//組織aに必要な人員がいなかったらa[i]のうち全員を出す。
+        }
+
+        if(sum >= k*mid) l = mid;
+        else r = mid;
+    }
+
+    cout<<l<<endl;
+}
